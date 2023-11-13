71283e39b5ee69fbedfd6692eb3b08abe0a3eee0
cuong914 <126068690+cuong914@users.noreply.github.com>
2023-11-13 23:26:03 +0700

    Add files via upload

--- src/duan1_bangiay/View/Voucher.java
+++ src/duan1_bangiay/View/Voucher.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package da_bangiay.View;
+package duan1_bangiay.View;
 
 /**
  *
