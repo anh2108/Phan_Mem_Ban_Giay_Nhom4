7af74d52e818868c572b9a9d35648d68120efa7f
Hoai Thu <149553436+thunh104@users.noreply.github.com>
2023-11-13 13:09:54 +0700

    Update DiaChi

--- src/duan1_bangiay/Model/DiaChi.java
+++ src/duan1_bangiay/Model/DiaChi.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package duan1_bangiay.Model;
-
-/**
- *
- * @author HoaiThu
- */
 public class DiaChi {
     
 }
