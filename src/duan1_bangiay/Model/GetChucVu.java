dfee131f8e064a32fb96152e0180a6b4b674bfbc
anh2108 <118011163+anh2108@users.noreply.github.com>
2023-11-16 04:44:30 +0700

    Update GetChucVu.java

--- src/duan1_bangiay/Model/GetChucVu.java
+++ src/duan1_bangiay/Model/GetChucVu.java
@@ -9,5 +9,32 @@ package duan1_bangiay.Model;
  * @author Admin
  */
 public class GetChucVu {
+    private static String Manv;
+    private static String matkhau;
+    private static int chucvu;
+
+    public static String getManv() {
+        return Manv;
+    }
+
+    public static void setManv(String Manv) {
+        GetChucVu.Manv = Manv;
+    }
+
+    public static String getMatkhau() {
+        return matkhau;
+    }
+
+    public static void setMatkhau(String matkhau) {
+        GetChucVu.matkhau = matkhau;
+    }
+
+    public static int getChucvu() {
+        return chucvu;
+    }
+
+    public static void setChucvu(int chucvu) {
+        GetChucVu.chucvu = chucvu;
+    }
     
 }
